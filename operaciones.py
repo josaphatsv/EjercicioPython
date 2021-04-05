eed4adffdcc4d31b0b94bbc5fbfff5faf0c3f4ec
Josaphat Lopez Ceron <josa1980@gmail.com>
2021-04-05 10:58:54 -0600

    operaciones con float

---
+++ operaciones.py
@@ -0,0 +1,15 @@
+# ingresa un valor flotante para la variable a aquí
+num1=float(input("Ingrese primer numero con decimal: "))
+# ingresa un valor flotante para la variable b aquí
+num2=float(input("Ingrese segundo numero con decimal: "))
+# muestra el resultado de la suma aquí 
+suma=num1+num2
+print("El total de la suma es: "+str(suma))
+# muestra el resultado de la resta aquí
+print("El total de la resta es: "+str(num1-num2))
+# muestra el resultado de la multiplicación aquí
+print("El total de la multiplicacion es: "+str(num1*num2))
+# muestra el resultado de la división aquí
+print("El total de la divicion es:"+str(num1/num2))
+
+print("\n¡Eso es todo, amigos!")
