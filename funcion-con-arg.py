9fac30b8e57bbc7ce1a65d3a71b50e34cf3be9d9
Josaphat Lopez Ceron <josa1980@gmail.com>
2021-04-05 11:10:24 -0600

    funcion con argumento

---
+++ funcion-con-arg.py
@@ -0,0 +1,7 @@
+#funcion con parametros
+def funcion_arg(nombre,apellido):
+    print("El nombre recibido es:", nombre)
+    print("El nombre recibido es:", apellido)
+    
+
+funcion_arg("Josaphat","Lopez")
