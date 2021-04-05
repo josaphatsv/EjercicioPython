a5d3ee4d2115b8842ab09e2480578138beb1437a
Josaphat Lopez Ceron <josa1980@gmail.com>
2021-04-05 11:03:24 -0600

    ejercicio de import python

---
+++ test_artimetica.py
@@ -0,0 +1,8 @@
+from Modulos import modulo_aritmetica as aritmetica
+#import Modulos.modulo_aritmetica as aritmetica
+
+resultado=aritmetica.sumar(1,2)
+print(resultado)
+
+resultado=aritmetica.restar(3,1)
+print(resultado)
