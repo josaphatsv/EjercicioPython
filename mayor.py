3405fe508237093f77f351258a356dc91adb26cf
Josaphat Lopez Ceron <josa1980@gmail.com>
2021-04-06 18:56:20 -0600

    Ejercicio de Mayor que

---
+++ mayor.py
@@ -0,0 +1,6 @@
+num1=int(input("Proporciona el numero1: "))
+num2=int(input("Proporciona el numero2: "))
+if num1>num2:
+    print("El numero mayor es:",num1)
+else:
+    print("El numero mayor es:",num2)
